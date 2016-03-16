a10d6a8e631e90976475c7946a61f988218d3ae5
kaiyangjia <kaiyang.jia@ourwill.cn>
2016-03-16 18:27:22 +0800

    init

---
+++ app/src/main/java/com/jiakaiyang/photoshare/service/LoadPhotoService.java
@@ -0,0 +1,7 @@
+package com.jiakaiyang.photoshare.service;
+
+/**
+ * Created by kaiyangjia on 2016/3/16.
+ */
+public class LoadPhotoService {
+}
--- app/src/main/java/com/jiakaiyang/photoshare/ui/BaseActivity.java
+++ app/src/main/java/com/jiakaiyang/photoshare/ui/BaseActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by kaiyangjia on 2016/3/16.
+ *
  */
 public class BaseActivity extends AppCompatActivity {
 }
