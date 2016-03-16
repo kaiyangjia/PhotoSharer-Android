b7a664f550054e7bf9f24f4227656d15919e7ff3
kaiyangjia <kaiyang.jia@ourwill.cn>
2016-03-16 18:02:31 +0800

    init

---
+++ app/src/main/java/com/jiakaiyang/photoshare/ui/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.jiakaiyang.photoshare.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kaiyangjia on 2016/3/16.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
